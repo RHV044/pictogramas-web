8e6288dedd59f60535ce1f0efe798df710052b8e
Leonardo <26606912+orlandoleonardo@users.noreply.github.com>
2022-07-25 19:17:11 -0300

    avance en configuracion

--- src/configuracion/configuracion.tsx
+++ src/configuracion/configuracion.tsx
@@ -1,11 +1,43 @@
 import ResponsiveAppBar from "../commons/appBar";
+import SettingsIcon from '@mui/icons-material/Settings';
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export default function Configuracion(){
 
   return(
     <div>
       <ResponsiveAppBar />
-      Configuracion
+      <div >
+        <h1> <SettingsIcon color="action"/> Configuracion</h1>
+        <FormGroup>
+          <FormControlLabel
+            control = {
+                 <Switch/>
+            } label= "Permitir Contenido violento"
+          />
+          <FormControlLabel
+            control = {
+                 <Switch/>
+            } label= "Permitir Contenido sexual"
+          />
+          <FormControlLabel
+            control = {
+                 <Switch/>
+            } label= "Skin"
+          />
+          <FormControlLabel
+            control = {
+                 <Switch/>
+            } label= "Hair"
+          />
+          
+        </FormGroup>
+        
+
+      </div>
+      
     </div>
   )
 }
