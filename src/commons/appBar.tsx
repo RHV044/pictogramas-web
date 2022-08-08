aae2ab01f562d0ff08300d6d6d3673d5c802c9ec
Gonzalo Vaquero <gonzovaq@gmail.com>
2022-08-08 20:56:39 -0300

    PictogAR

--- src/commons/appBar.tsx
+++ src/commons/appBar.tsx
@@ -62,7 +62,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            PictogAR
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
