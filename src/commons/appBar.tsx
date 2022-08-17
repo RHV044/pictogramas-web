55a22b1bab08016009244fbf834198a3f25f8026
Gonzalo Vaquero <gonzovaq@gmail.com>
2022-08-17 17:11:16 -0300

    PictogAR en vez de logo

--- src/commons/appBar.tsx
+++ src/commons/appBar.tsx
@@ -121,7 +121,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            PictogAR
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
--- src/pizarras/movimientos.tsx
+++ src/pizarras/movimientos.tsx
@@ -5,6 +5,7 @@ export type Position = {
   fila: number,
   columna: number
 }
+
 export type Grafico = {
   texto: string, 
   posicion: Position,
