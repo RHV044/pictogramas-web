ef16624f75760a03cbe78f954c221d937216e9a5
Gonzalo Vaquero <gonzovaq@gmail.com>
2022-10-21 16:41:26 -0300

    agrego estadisticas (requiere obligatoriamente conexion)

--- src/estadisticas/estadisticas.tsx
+++ src/estadisticas/estadisticas.tsx
@@ -1,9 +1,12 @@
+import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 import ResponsiveAppBar from "../commons/appBar";
 import { getUsuarioLogueado, ObtenerEstadisticas } from "../services/usuarios-services";
+import CategoriasMasUtilizadas from "./categoriasMasUtilizadas";
+import PictogramasMasUtilizados from "./pictogramasMasUtilizados";
 
 export default function Estadisticas(){
-  const [estadisticas, setEstadisticas] = useState([] as  any[])
+  const [estadisticas, setEstadisticas] = useState(null as  any)
 
   useEffect(() => {
     dispatchEvent(new CustomEvent('sincronizar'));
@@ -13,10 +16,24 @@ export default function Estadisticas(){
     })
   }, [])
 
-  return(
+  return (
     <div>
       <ResponsiveAppBar />
       Estadisticas
+      {estadisticas && (
+        <>
+          <Box sx={{ display: 'flex' }}>
+            Pictogramas Mas Utilizados
+            <PictogramasMasUtilizados pictogramas={estadisticas.pictogramasMasUtilizados} />
+          </Box>
+          <Box sx={{ display: 'flex' }}> Total Pictogramas distintos utilizados: {estadisticas.cantidadDePictogramasDistintosUtilizados} </Box>
+          <Box sx={{ display: 'flex' }}>
+            Categorias Mas Utilizadas
+            <CategoriasMasUtilizadas categorias={estadisticas.categoriasMasUtilizadas}/>
+          </Box>
+          <Box sx={{ display: 'flex' }}> Total Categorias distintas utilizadas: {estadisticas.cantidadDeCategoriasDistintasUtilizadas} </Box>
+        </>
+      )}
     </div>
-  )
+  );
 }
---
+++ src/estadisticas/pictogramasMasUtilizados.tsx
@@ -0,0 +1,99 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  Grid,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { IPictogram } from '../pictogramas/models/pictogram';
+import {
+  ObtenerPictogramasConImagenes,
+} from '../pictogramas/services/pictogramas-services';
+
+export default function PictogramasMasUtilizados(props: any) {
+  const [pictogramas, setPictogramas] = useState([] as IPictogram[]);
+
+  useEffect(() => {
+    ObtenerPictogramasConImagenes(props.pictogramas).then((picts) => {
+      setPictogramas(picts);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 10, md: 12 }}
+      >
+        {pictogramas.map((pictograma) => {
+          return (
+            <Grid
+              key={pictograma.id ? pictograma.id : pictograma.identificador}
+              item
+              xs={12}
+              sm={4}
+              md={2}
+            >
+              <Container
+                key={pictograma.id ? pictograma.id : pictograma.identificador}
+              >
+                <Card
+                  sx={{
+                    maxWidth: 230,
+                    minWidth: 70,
+                    maxHeight: 240,
+                    minHeight: 50,
+                  }}
+                  style={{ marginTop: '10px' }}
+                  onClick={() => {}}
+                >
+                  <CardActionArea
+                    onClick={() => {
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      height="160"
+                      width="140"
+                      src={
+                        pictograma.imagen &&
+                        pictograma.imagen.includes('data:image')
+                          ? pictograma.imagen
+                          : `data:image/png;base64,${pictograma.imagen}`
+                      }
+                      alt={pictograma.keywords[0].keyword}
+                    ></CardMedia>
+                    <CardHeader
+                      style={{
+                        height: '100%',
+                        width: '95%',
+                        marginBottom: 1,
+                        paddingBottom: 0,
+                      }}
+                    ></CardHeader>
+                    <CardContent
+                      style={{
+                        marginTop: 1,
+                        paddingTop: 0,
+                        marginLeft: 4,
+                        paddingLeft: 0,
+                        fontWeight: 'bold',
+                        paddingBottom: 0,
+                      }}
+                    >
+                      {pictograma.keywords[0].keyword.toLocaleUpperCase()}
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Container>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+}
