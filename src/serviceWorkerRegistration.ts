83afbe75c563b4bc9496297a4ea993ab16d8171c
Gonzalo Vaquero <gonzovaq@gmail.com>
2022-11-18 20:20:43 -0300

    pruebo modificaciones en serviceworker

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -27,8 +27,10 @@ export function register(config?: Config) {
   console.log("Register service worker")
   console.log("node_env: ", process.env.NODE_ENV)
   console.log("service worker en navigation: ", 'serviceWorker' in navigator)
+  // 'serviceWorker' in navigator si no es https, es false y no entra
   // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    if ('serviceWorker' in navigator) {
+    // if ('serviceWorker' in navigator) {
+      if (true){
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     console.log("public url:", publicUrl)
