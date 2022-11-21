df881241d1ee5c0c4d95fc969996d3dcf03e4f63
Leonardo <26606912+orlandoleonardo@users.noreply.github.com>
2022-11-21 01:00:12 -0300

    arreglo favs

--- src/services/update-service.ts
+++ src/services/update-service.ts
@@ -639,8 +639,8 @@ export class UpdateService {
                   ) {
                     const favCompleto: IFavoritoPorUsuario = {
                       id: favorito.id,
-                      idUsuario: favorito.usuarioId,
-                      idPictograma: favorito.pictogramaId,
+                      idUsuario: favorito.idUsuario,
+                      idPictograma: favorito.idPictograma,
                       pendienteAgregar: false,
                       pendienteEliminar: false,
                     };
